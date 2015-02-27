a634571161b3848840c69110b89eb0193fa0fade
tylin <tl483@cornell.edu>
2015-02-27 17:00:22 -0500

    major revision for code cleanup and performance improvement

--- codalab/program/caption_eval/evals.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'tylin'
-
-from tokenizer.ptbtokenizer import PTBTokenizer
-from bleu.bleu import Bleu
-from meteor.meteor import Meteor
-from rouge.rouge import Rouge
-# from cider.cider import Cider
