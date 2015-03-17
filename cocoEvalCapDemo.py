0b1899759be002f76526c1cb2eb5e665bfe031a2
tylin <tl483@cornell.edu>
2015-03-17 19:29:51 -0400

    update demo

--- cocoEvalCapDemo.py
+++ cocoEvalCapDemo.py
@@ -17,6 +17,7 @@ encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 
 # In[2]:
 
+# set up file names and pathes
 dataDir='.'
 dataType='val2014'
 algName = 'fakecap'
@@ -27,25 +28,36 @@ subtypes=['results', 'evalImgs', 'eval']
 
 # In[3]:
 
+# instantiate coco object and cocoRes object
 coco = COCO(annFile)
 cocoRes = coco.loadRes(resFile)
 
 
 # In[4]:
 
+# instantiate cocoEval object by taking coco and cocoRes
+
 cocoEval = COCOEavlCap(coco, cocoRes)
+
+# evaluate on a subset of images by setting
+# cocoEval.params['image_id'] = cocoRes.getImgIds()
+# please remove this line when evaluating the full validation set
 cocoEval.params['image_id'] = cocoRes.getImgIds()
+
+# evalute results
 cocoEval.evaluate()
 
 
 # In[5]:
 
+# print output evaluation scores
 for metric, score in cocoEval.eval.items():
     print '%s: %.3f'%(metric, score)
 
 
 # In[6]:
 
+# demo how to use evalImgs to retrieve low score result
 evals = [eva for eva in cocoEval.evalImgs if eva['CIDEr']<30]
 print 'ground truth captions'
 imgId = evals[0]['image_id']
@@ -53,7 +65,7 @@ annIds = coco.getAnnIds(imgIds=imgId)
 anns = coco.loadAnns(annIds)
 coco.showAnns(anns)
 
-print '==================================================='
+print '\n'
 print 'generated caption (CIDEr score %0.1f)'%(evals[0]['CIDEr'])
 annIds = cocoRes.getAnnIds(imgIds=imgId)
 anns = cocoRes.loadAnns(annIds)
@@ -62,18 +74,24 @@ coco.showAnns(anns)
 img = coco.loadImgs(imgId)[0]
 I = io.imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']))
 plt.imshow(I)
+plt.axis('off')
 plt.show()
 
 
 # In[7]:
 
+# plot score histogram
 ciderScores = [eva['CIDEr'] for eva in cocoEval.evalImgs]
 plt.hist(ciderScores)
+plt.title('Histogram of CIDEr Scores', fontsize=20)
+plt.xlabel('CIDEr score', fontsize=20)
+plt.ylabel('result counts', fontsize=20)
 plt.show()
 
 
 # In[8]:
 
+# save evaluation results to ./results folder
 json.dump(cocoEval.evalImgs, open(evalImgsFile, 'w'))
 json.dump(cocoEval.eval,     open(evalFile, 'w'))
 
