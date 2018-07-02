67d15c24a9d552972c97d624a07c38c83fc671cb
Dylan Flaute <dylan.flaute@keywcorp.com>
2018-07-02 16:41:13 -0400

    Add build instructions to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
+import subprocess
 from distutils.core import setup
+
+bash_command = "./get_stanford_models.sh"
+
+
+subprocess = subprocess.Popen(bash_command, cwd=os.path.dirname(os.path.realpath(__file__)))
 
 setup(
     name="pycocoevalcap"
