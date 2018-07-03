0aacba8b89def08b663c1492e1cf1387472548c9
Dylan Flaute <dylan.flaute@keywcorp.com>
2018-07-03 16:13:10 -0400

    Switch get_stanford from sh to python.
    The entire script can be done with the Python
    standard library, which is better than relying on
    Unix programs.

--- setup.py
+++ setup.py
@@ -2,22 +2,11 @@ import sys
 import os
 import subprocess
 from setuptools import setup, find_packages
+import get_stanford_models
 
 bash_command = "./get_stanford_models.sh"
 
-curdir = os.path.dirname(os.path.realpath(__file__))
-spicedir = os.path.join(curdir, "spice", "lib")
-stanford_fmt = os.path.join(spicedir, "stanford-corenlp-3.6.0%s.jar")
-base_jar = stanford_fmt % ""
-model_jar = stanford_fmt % "-models"
-# if os.path.exists(base_jar):
-#     os.remove(base_jar)
-# if os.path.exists(model_jar):
-#     os.remove(model_jar)
-
-if subprocess.call(bash_command, cwd=curdir) != 0:
-    print("Stanford Jars could not be obtained and built. Do you have zip?")
-    sys.exit(1)
+get_stanford_models.main()
 
 setup(
     name="pycocoevalcap",
