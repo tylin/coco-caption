fcd441654da02a1dd6e573aabefd195ce349e899
Dylan Flaute <dylan.flaute@keywcorp.com>
2018-07-03 10:18:32 -0400

    Make setup.py more robust.

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+import sys
 import os
 import subprocess
 from distutils.core import setup
 
 bash_command = "./get_stanford_models.sh"
 
+curdir = os.path.dirname(os.path.realpath(__file__))
+spicedir = os.path.join(curdir, "spice", "lib")
+stanford_fmt = os.path.join(spicedir, "stanford-corenlp-3.6.0%s.jar")
+base_jar = stanford_fmt % ""
+model_jar = stanford_fmt % "-models"
+if os.path.exists(base_jar):
+    os.remove(base_jar)
+if os.path.exists(model_jar):
+    os.remove(model_jar)
 
-subprocess = subprocess.Popen(bash_command, cwd=os.path.dirname(os.path.realpath(__file__)))
+if subprocess.call(bash_command, cwd=curdir) != 0:
+    print("Stanford Jars could not be obtained and built. Do you have zip?")
+    sys.exit(1)
 
 setup(
     name="pycocoevalcap"
 )
+
