e197791ce80943904b37f26580bbc428ae52b1d7
Dylan Flaute <dylan.flaute@keywcorp.com>
2018-07-24 10:54:46 -0400

    Update to pull pip pycoco

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ import subprocess
 from setuptools import setup, find_packages
 import get_stanford_models
 
-bash_command = "./get_stanford_models.sh"
-
 get_stanford_models.main()
 
 setup(
     name="pycocoevalcap",
     version="0.0",
-    packages=find_packages(),
+    packages=["pycocoevalcap"],
     zip_safe=False,
-    include_package_data=True
+    include_package_data=True,
+    install_requires=['pycocotools']
 )
 
